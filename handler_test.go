5685fcd7c21c219a289892abf50c98c6f7d6f187
ValSpp <muhammadnovals334@gmail.com>
2024-01-21 13:05:24 +0700

    httpTest

--- handler_test.go
+++ handler_test.go
@@ -47,3 +47,20 @@ func TestServerMux(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestRequest(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, request.Method)
+		fmt.Fprint(writer, request.RequestURI)
+	}
+
+	server := http.Server{
+		Addr: "localhost: 8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err !=nil {
+		panic(err)
+	}
+}
